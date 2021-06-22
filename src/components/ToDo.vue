651acb3415ca4d1d96711c6f9c8bd0b78e5167b8
ismail9k <dev@ismail9k.com>
2021-06-22 12:59:47 +0300

    feat: add todo functionality

--- src/components/ToDo.vue
+++ src/components/ToDo.vue
@@ -4,20 +4,31 @@
       <li class="todo__item" v-for="item in items" :key="item">{{ item }}</li>
     </ul>
 
-    <input type="text" class="todo__input" />
-    <button class="todo__submit">add</button>
+    <input type="text" class="todo__input" v-model="inputValue" />
+    <button class="todo__submit" @click="handleButtonSubmit">add</button>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
 
 export default defineComponent({
   name: 'ToDo',
-  props: {
-    items: {
-      type: Array,
-    },
+  setup() {
+    const items = ref<Array<string>>([]);
+    const inputValue = ref<string>('');
+
+    function handleButtonSubmit() {
+      if (!inputValue.value.length) return;
+      items.value.push(inputValue.value);
+      inputValue.value = '';
+    }
+
+    return {
+      items,
+      inputValue,
+      handleButtonSubmit,
+    };
   },
 });
 </script>
--- tests/unit/todo.spec.ts
+++ tests/unit/todo.spec.ts
@@ -24,4 +24,16 @@ describe('ToDo.vue', () => {
     expect(button.exists()).toBe(true);
     expect(button.text()).toBe('add');
   });
+
+  it('Adds new todo items', async () => {
+    const input = wrapper.find('.todo__input');
+    const button = wrapper.find('.todo__submit');
+
+    await input.setValue('buy some milk');
+    await button.trigger('click');
+
+    const items = wrapper.findAll('ul > li');
+    expect(items.length).toBe(1);
+    expect(items[0].text()).toBe('buy some milk');
+  });
 });
